07d938766bf96b2db3bccbfb70242537c68d1b56
Larissa-P-B <126874430+Larissa-P-B@users.noreply.github.com>
2024-03-04 16:02:06 -0300

    inicio do projeto

---
+++ TheRisingSun/README.md
@@ -0,0 +1,17 @@
+# Projeto-TheRisingSun
+
+API do projeto TheRisingSun - E-commerce Papelaria
+
+## Tarefas
+
+- [ ] CRUD de Papelaria
+- [ ] CRUD de Materiais de Escritório
+- [ ] CRUD de Informática
+- [ ] CRUD de Carrinho de Compras
+- [ ] CRUD de Cadastro de Cliente
+
+## Documentação da API
+
+### ENDPOINT
+
+
---
+++ TheRisingSun/src/main/java/br/com/fiap/therisingsun/TheRisingSun/TheRisingSunApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.therisingsun.TheRisingSun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRisingSunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRisingSunApplication.class, args);
+	}
+
+}
