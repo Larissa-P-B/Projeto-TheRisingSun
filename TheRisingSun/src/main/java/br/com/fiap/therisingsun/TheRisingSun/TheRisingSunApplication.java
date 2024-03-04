b6853d0cede9ff195add57cab3e5e7dc76650284
Larissa-P-B <126874430+Larissa-P-B@users.noreply.github.com>
2024-03-04 16:25:31 -0300

    projeto inicio

--- TheRisingSun/src/main/java/br/com/fiap/therisingsun/TheRisingSun/TheRisingSunApplication.java
+++ TheRisingSun/src/main/java/br/com/fiap/therisingsun/TheRisingSun/TheRisingSunApplication.java
@@ -2,12 +2,22 @@ package br.com.fiap.therisingsun.TheRisingSun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class TheRisingSunApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheRisingSunApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String home(){
+		return "The Rising Sun - Papelaria Online";
 	}
 
 }
