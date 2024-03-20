daf977bf7a568f8af3ebe23b34b3e401c68f5832
Larissa-P-B <126874430+Larissa-P-B@users.noreply.github.com>
2024-03-20 19:17:59 -0300

    atualização de Produto e Categoria Controller

--- src/main/java/br/com/fiap/therisingsun/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/therisingsun/controller/ProdutoController.java
@@ -2,20 +2,28 @@ package br.com.fiap.therisingsun.controller;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-
+import br.com.fiap.therisingsun.model.Categoria;
 import br.com.fiap.therisingsun.model.Produto;
 
 import br.com.fiap.therisingsun.repository.ProdutoRepository;
@@ -50,6 +58,32 @@ public class ProdutoController {
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
 
+    }
+
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        log.info("apagando categoria {}", id);
+        verificarSeCategoriaExiste(id);
+        repository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public Categoria update(@PathVariable Long id, @RequestBody Categoria categoria) {
+        log.info("atualizar categoria {} para {}", id, categoria);
+
+        verificarSeCategoriaExiste(id);
+        categoria.setId(id);
+        return repository.save(categoria);
+    }
+
+    private void verificarSeCategoriaExiste(Long id) {
+        repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        NOT_FOUND,
+                        "Não existe categoria com o id informado"));
     }
 
 }
