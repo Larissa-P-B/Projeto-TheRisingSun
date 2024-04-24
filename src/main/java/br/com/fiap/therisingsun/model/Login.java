5e555ba13558a55af2f7996c4fd6b4ca51c83b55
Larissa-P-B <126874430+Larissa-P-B@users.noreply.github.com>
2024-04-24 17:35:41 -0300

    atualização

--- src/main/java/br/com/fiap/therisingsun/model/Login.java
+++ src/main/java/br/com/fiap/therisingsun/model/Login.java
@@ -1,6 +1,6 @@
 package br.com.fiap.therisingsun.model;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/br/com/fiap/therisingsun/model/StatusPedido.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.therisingsun.model;
-
-public enum StatusPedido {
-    AGUARDANDO_PAGAMENTO,
-    PROCESSANDO,
-    ENVIADO,
-    ENTREGUE,
-    CANCELADO
-}
